bc41772025b42c76bbeb03dc2091fc3dedb1d6b2
Mazy <mazy_ios@163.com>
2019-07-10 14:16:31 +0800

    feat: add lib files structure

---
+++ IMScrollPageView/IMScrollPageViewLib/IMSegmentStyle.swift
@@ -0,0 +1,13 @@
+//
+//  IMSegmentStyle.swift
+//  IMScrollPageView
+//
+//  Created by Mazy on 2019/7/10.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import Foundation
+
+public struct IMSegmentStyle {
+    
+}
---
+++ IMScrollPageView/IMScrollPageViewLib/UIViewController+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Extension.swift
+//  IMScrollPageView
+//
+//  Created by Mazy on 2019/7/10.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+}
